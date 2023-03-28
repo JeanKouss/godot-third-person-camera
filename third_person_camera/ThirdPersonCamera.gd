1cc9a6c389f4a4076f6b4975d1cfe39177aac860
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2023-03-28 21:51:43 +0000

    camera exported variables added but not linked

--- third_person_camera/ThirdPersonCamera.gd
+++ third_person_camera/ThirdPersonCamera.gd
@@ -1,12 +1,11 @@
 @tool
 extends Node3D
 
-@onready var _camera := $Camera
-@onready var _camera_rotation_pivot = $RotationPivot
-@onready var _camera_offset_pivot = $RotationPivot/OffsetPivot
-@onready var _camera_spring_arm := $RotationPivot/OffsetPivot/CameraSpringArm
-@onready var _camera_marker := $RotationPivot/OffsetPivot/CameraSpringArm/CameraMarker
 
+
+
+
+@export_category("allo")
 @export var distance_from_pivot := 10.0
 
 @export var pivot_offset := Vector2.ZERO 
@@ -22,8 +21,28 @@ extends Node3D
 @export_range(-90.0, 90.0) var tilt_lower_limit_deg := -60.0
 
 @export_range(1.0, 100.0) var tilt_sensitiveness := 10.0
-@export_range(1.0, 100.0) var horizontal_rotation_sensitiveness := 100.0
+@export_range(10.0, 700.0) var horizontal_rotation_sensitiveness := 100.0
 
+# Exported variables to setup the camera itself
+@export var current : bool = false
+
+@export_category("Camera3D")
+@export var keep_aspect : Camera3D.KeepAspect = Camera3D.KEEP_HEIGHT
+@export_flags_3d_render var cull_mask :int 
+@export var environment : Resource
+@export var attributes : Resource
+@export var doppler_tracking : Camera3D.DopplerTracking = Camera3D.DOPPLER_TRACKING_DISABLED
+@export var projection : Camera3D.ProjectionType = Camera3D.PROJECTION_PERSPECTIVE
+@export var f_o_v = 75
+@export var near := 0.05
+@export var far := 4000.0
+
+
+@onready var _camera := $Camera
+@onready var _camera_rotation_pivot = $RotationPivot
+@onready var _camera_offset_pivot = $RotationPivot/OffsetPivot
+@onready var _camera_spring_arm := $RotationPivot/OffsetPivot/CameraSpringArm
+@onready var _camera_marker := $RotationPivot/OffsetPivot/CameraSpringArm/CameraMarker
 
 
 var camera_tilt_deg := 0.0
@@ -37,22 +56,22 @@ func _process(delta):
 	_camera_spring_arm.spring_length = distance_from_pivot
 	_camera_offset_pivot.global_position = _camera_offset_pivot.get_parent().global_position + Vector3(pivot_offset.x, pivot_offset.y, 0.0)
 	_camera_rotation_pivot.global_rotation_degrees.x = initial_dive_angle_deg
-	process_tilt_input()
-	process_horizontal_rotation_input()
-	update_camera_tilt()
-	update_camera_horizontal_rotation()
+	_process_tilt_input()
+	_process_horizontal_rotation_input()
+	_update_camera_tilt()
+	_update_camera_horizontal_rotation()
 
 
 
 
-func process_horizontal_rotation_input() :
+func _process_horizontal_rotation_input() :
 	if InputMap.has_action("tp_camera_right") and InputMap.has_action("tp_camera_left") :
 		var camera_horizontal_rotation_variation = Input.get_action_strength("tp_camera_right") -  Input.get_action_strength("tp_camera_left")
 		camera_horizontal_rotation_variation = camera_horizontal_rotation_variation * get_process_delta_time() * horizontal_rotation_sensitiveness
 		camera_horizontal_rotation_deg += camera_horizontal_rotation_variation
 
 
-func process_tilt_input() :
+func _process_tilt_input() :
 	if InputMap.has_action("tp_camera_up") and InputMap.has_action("tp_camera_down") :
 		var tilt_variation = Input.get_action_strength("tp_camera_up") -  Input.get_action_strength("tp_camera_down")
 		tilt_variation = tilt_variation * get_process_delta_time() * tilt_sensitiveness
@@ -60,10 +79,10 @@ func process_tilt_input() :
 	
 
 
-func update_camera_tilt() :
+func _update_camera_tilt() :
 	_camera.global_rotation_degrees.x = clampf(initial_dive_angle_deg + camera_tilt_deg, tilt_lower_limit_deg, tilt_upper_limit_deg)
 
-func update_camera_horizontal_rotation() :
+func _update_camera_horizontal_rotation() :
 	# TODO : inverse
 	_camera_rotation_pivot.global_rotation_degrees.y = camera_horizontal_rotation_deg * -1
 	_camera.global_rotation_degrees.y = camera_horizontal_rotation_deg * -1
